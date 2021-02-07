c3ab07cc68832cb2425527f644d0aa39c923c805
Alberto Bertollo <66118879+ghillahill@users.noreply.github.com>
2021-02-07 23:12:08 +0100

    Init Vue CLI with Tailwind CSS

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+      <div class="card">
+        <h1 class="font-bold text-9xl">Component</h1>
+        <h2 class="text-3xl font-bold">
+            Using VUE CLI
+        </h2>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    @apply flex justify-center items-center h-screen text-center text-emerald-500 bg-gray-50;
+}
+.card{
+    @apply rounded-lg border border-emerald-300 bg-emerald-100 p-10 shadow-lg;
+
+    h2{
+       @apply p-5;
+    }
+}
+</style>
